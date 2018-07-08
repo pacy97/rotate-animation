ff43afe73d83cec2b2f9552308b109fdc25dd111
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-08 18:02:18 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -8,20 +8,34 @@ import android.support.v7.app.AppCompatActivity;
  * Created by sumith on 16-10-2017.
  */
 
-public class about extends AppCompatActivity {
-
-private Button button;
-
+public class SimpleCamera extends AppCompatActivity {
+    EditText ed1;
+    Button btn,btn1,btn2,btn3,btn4;
+    String string;
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
-        button=(TextView)findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_simple_camera);
+        btn=(Button)findViewById(R.id.btn);
+        btn1=(Button)findViewById(R.id.btn1);
+   
+        
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-               //Add your operation here.......
+            public void onClick(View v) {
+            ImageView imageView=(ImageView)findViewById(R.id.imv);
+                Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.clockwise);
+                imageView.startAnimation(animation);
             }
-        });
-    }
+    });
+         
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            ImageView imageView=(ImageView)findViewById(R.id.imv);
+                Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anti_clockwise);
+                imageView.startAnimation(animation);
+            }
+    });
+}
 }
